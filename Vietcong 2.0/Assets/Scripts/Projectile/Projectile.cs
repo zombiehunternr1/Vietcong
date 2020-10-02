c102a34e011731409525a13d2bfc8f91123246d3
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-10-02 15:52:41 +0200

    Projectiles added
    Added in a projectile shooter and a projectile that moves across the map once instantiated. Need to collision to the projectile when getting hit.

---
+++ Vietcong 2.0/Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float MovementSpeed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * MovementSpeed;
+    }
+}
---
+++ Vietcong 2.0/Assets/Scripts/Projectile/Projectileshooter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectileshooter : MonoBehaviour
+{
+    public GameObject Projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var newProjectile = Instantiate(Projectile, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        newProjectile.transform.parent = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
