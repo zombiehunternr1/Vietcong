bf24f5fc53533fd01573674964b7b47e586eb33e
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-10-08 14:05:59 +0200

    Tiledrop pattern
    Added in a Tile Manager script that keeps track of how many tiles are currently active. The tiles all have a tile script that, once apon called, give the player feedback that the tile is about the fall. The tile manager keeps looping over the tiles in the list until there are no more tiles available.

---
+++ Vietcong 2.0/Assets/Scripts/Hazards/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private Color COrange = new Color32(254, 161, 0, 1);
+
+    public IEnumerator DroppingThisTile()
+    {
+        //Gets the material from the selected tile that is stored in the variable DTile.
+        var DTileColor = GetComponent<Renderer>().material;
+        //Changes the color to orange.
+        DTileColor.color = COrange;
+        //Waits 1 seconds before executing the rest of the code.
+        yield return new WaitForSeconds(1);
+        //Changes the color to red.
+        DTileColor.color = Color.red;
+        //Waits 1 seconds before executing the rest of the code.
+        yield return new WaitForSeconds(1);
+        //Destroys the tile that is stored in the variable DTile.
+        Destroy(this.gameObject);
+    }
+}
---
+++ Vietcong 2.0/Assets/Scripts/Hazards/TileManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    private List<Transform> TileList = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //creates an array TempTile and gets all the children components that are attached to this parent.
+        Transform[] TempTile = GetComponentsInChildren<Transform>();
+
+        //Goes over each indivitual tile in the array TempTile;
+        foreach(Transform tile in TempTile)
+        {
+            //Adds the tile to the list TileList.
+            TileList.Add(tile);
+        }
+        //Removes the first tile in the array because this is the parent tile. This tile must not be removed else the code will break.
+        TileList.RemoveAt(0);
+        //Starts the coroutine that makes the tiles drop overtime.
+        StartCoroutine(DropTile());
+    }
+
+    public IEnumerator DropTile()
+    {
+        //Keeps looping until the amount of tiles in the TileList is smaller then 0.
+        while (TileList.Count > 0)
+        {
+            //Gets a random tile from the TileList and puts it in the variable DTile.
+            var DropThisTile = TileList[Random.Range(0, TileList.Count)];
+            yield return new WaitForSeconds(1);
+            //Starts the coroutine that drops the single selected tile.
+            StartCoroutine(DropThisTile.GetComponent<Tile>().DroppingThisTile());
+            //Removes the tile that is stored in the variable DTile from the list TileList.
+            TileList.Remove(DropThisTile);
+        }
+    }
+}
