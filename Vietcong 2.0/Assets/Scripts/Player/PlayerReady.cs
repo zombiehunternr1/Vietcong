386a7aff94a628a486d5954311e8866b9e086a51
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-10-26 20:31:26 +0100

    Readyup system
    Player can now ready up before starting the game. This now only works with one player. Need to test it with multiple players.

---
+++ Vietcong 2.0/Assets/Scripts/Player/PlayerReady.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerReady : MonoBehaviour
+{
+    public BoolReference Ready;
+    public Text ReadyUI;
+
+    public PlayerReadyList ReadyList;
+
+    //Sets the value to false when starting the game.
+    private void Awake()
+    {
+        Ready.value = false;
+    }
+    //This function when called sets the ready boolean to true, changes the color from red to green and calls the function "CheckAllPlayersReady".
+    private void OnReadyPlayer()
+    {
+        Ready.value = true;
+        ReadyUI.color = Color.green;
+        ReadyList.CheckAllPlayersReady();
+    }
+}
