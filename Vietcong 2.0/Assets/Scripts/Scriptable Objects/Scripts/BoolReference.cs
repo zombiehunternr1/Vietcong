ad4424ce50096c915bae6813e3886a703c6c2fda
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-10-26 20:33:07 +0100

    Revert "Readyup system"
    This reverts commit 386a7aff94a628a486d5954311e8866b9e086a51.

--- Vietcong 2.0/Assets/Scripts/Scriptable Objects/Scripts/BoolReference.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ReadyPlayer", menuName = "ScriptableObjects/ReadyPlayer")]
-public class BoolReference : ScriptableObject
-{
-    public bool value;
-}
