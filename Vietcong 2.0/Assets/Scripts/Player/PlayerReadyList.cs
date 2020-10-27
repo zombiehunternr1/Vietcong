68384f39dfe93cb09dc135e6e8708bdd205a9af8
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-10-27 16:44:38 +0100

    Added Ready UI
    Players can now ready up before the game starts. Need to fix the issue that all players need to be ready before the game starts instead of one.

--- Vietcong 2.0/Assets/Scripts/Player/PlayerReadyList.cs
+++ Vietcong 2.0/Assets/Scripts/Player/PlayerReadyList.cs
@@ -7,15 +7,22 @@ public class PlayerReadyList : MonoBehaviour
 {
     public List<BoolReference> Ready = new List<BoolReference>();
     public SetupGame StartGame;
+<<<<<<< Updated upstream
     public Text StartGameText;
+=======
+>>>>>>> Stashed changes
 
     //Function checks if all players are ready, if so it starts the game.
     public void CheckAllPlayersReady()
     {
         if (Ready.Count == PlayerTotal.PlayerList.Count)
         {
+<<<<<<< Updated upstream
             StartGameText.color = Color.green;
             //StartGame.ReadyGame();
+=======
+            StartGame.ReadyGame();
+>>>>>>> Stashed changes
         }      
     }
 }
