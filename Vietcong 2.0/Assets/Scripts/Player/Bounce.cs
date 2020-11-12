d93679f7b4bfcc2a10bd68069b5d566e115046c7
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-11-12 13:03:01 +0100

    Added bounce animation
    Added a bounce animation once two players hit eachother. Players can temporarely not move once the bounce animation is playing.

--- Vietcong 2.0/Assets/Scripts/Player/Bounce.cs
+++ Vietcong 2.0/Assets/Scripts/Player/Bounce.cs
@@ -10,6 +10,7 @@ public class Bounce : MonoBehaviour
     private const float DEFAULTSTUN = 0.25f;
     private AudioClip PlayBounceSound;
     private AudioSource BounceSource;
+    private Animator Anim;
 
     void Awake()
     {
@@ -17,6 +18,7 @@ public class Bounce : MonoBehaviour
         PlayBounceSound = BounceSource.clip;
         rb_Player = GetComponentInParent<Rigidbody>();
         handler = GetComponentInParent<StateHandler>();
+        Anim = GetComponentInParent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,11 +26,27 @@ public class Bounce : MonoBehaviour
         //Checks if the players collide with eachother
         if (PlayerTotal.PlayerList.Contains(other.gameObject))
         {
+            Anim.SetBool("IsBounce", true);
+            Anim.SetBool("IsRunning", false);
+            Anim.SetBool("IsMud", false);
+            other.GetComponent<Movement>()._canMove = false;
             BounceSource.PlayOneShot(PlayBounceSound, 0.7f);
             handler.SetCanAct(DEFAULTSTUN);
-            Vector3 direction = (transform.position - other.transform.position).normalized;
-
-            rb_Player.AddForce(direction * force, ForceMode.Impulse);
+            StartCoroutine(Delay(other.gameObject));
         }
+    }
+
+    IEnumerator Delay(GameObject other)
+    {
+        yield return new WaitForSeconds(0.35f);
+        Vector3 direction = (transform.position - other.transform.position).normalized;
+        rb_Player.AddForce(direction * force, ForceMode.Impulse);
+        yield return new WaitForSeconds(2.5f);
+        Anim.SetBool("IsBounce", false);
+        //Check if there is more the one player left after the bounce before allowing the player to move again.
+        if(PlayerTotal.PlayerList.Count != 1)
+        {
+            other.GetComponent<Movement>()._canMove = true;
+        }       
     }
 }
